e247f83e340770b4641b34ee6a744c0280d611a6
mateini@gmail.com <mateini@gmail.com>
2024-06-05 16:15:06 -0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
         match listener.accept().await {
             Ok((mut stream, addr)) => {
                 println!("Accepted new client: {:?}", addr);
-                handle_client(&mut stream).await?
+                tokio::spawn(async move {handle_client(&mut stream).await});
+                println!("After join")
             },
             Err(e) => println!("couldn't get client: {:?}", e),
         }
@@ -31,13 +32,13 @@ async fn main() -> Result<(), Box<dyn Error>> {
 }
 
 
-async fn handle_client(stream: &mut TcpStream) -> Result<(), Box<dyn Error>> {
+async fn handle_client(stream: &mut TcpStream) { // -> Result<(), Box<dyn Error>> {
     // let mut input_buffer = Vec::<u8>::with_capacity(128);
     // let mut empty_cnt = 0;
 
     loop {
         tokio::time::sleep(Duration::from_millis(20)).await;
-        stream.readable().await?;
+        stream.readable().await.unwrap();
 
         // let read_result = read_until_exhausted(&mut stream);
 
@@ -48,25 +49,27 @@ async fn handle_client(stream: &mut TcpStream) -> Result<(), Box<dyn Error>> {
         match stream.try_read_buf(&mut input_buffer) {
             Ok(0) => break,
             Ok(_) => {
-                println!("received {n_bytes}: {msg:?}", n_bytes=input_buffer.len(),
+                println!("received (from {addr}) {n_bytes}: {msg:?}",
+                         addr=stream.peer_addr().map(|a| a.to_string()).unwrap_or("<undefined>".to_string()),
+                         n_bytes=input_buffer.len(),
                          msg=dbgu::format_bytes_dbg(&input_buffer));
 
                 let write_result = stream.try_write("+PONG\r\n".as_bytes());
                 if let Err(err) = write_result {
                     println!("Error when writing: {err:?}");
-                    return Err(err.into())
+                    return // Err(err.into())
                 }
 
             }
             Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
                 continue;
             }
-            Err(e) => {
-                return Err(e.into());
+            Err(_e) => {
+                return; //  Err(e.into());
             }
         };
 
     }
-    Ok(())
+    // Ok(())
 }
 
